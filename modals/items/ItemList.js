0818386a0a8e43937d2c5b6cd7fa3ee2418d6bb2
Muhammad Haziq <muhammadhaziq341@gmail.com>
2020-11-08 16:24:08 +0500

    cahnegs in schema

--- modals/items/ItemList.js
+++ modals/items/ItemList.js
@@ -50,8 +50,23 @@ const itemListSchema = new mongoose.Schema({
   trackStock: {
     type: Boolean,
     default: false,
-    required: true
-  },  
+    required: true,
+  },
+  compositeItem: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  modifiersStatus: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  dsd: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
   stockQty: {
     type: String,
     max: 10000000,
@@ -59,88 +74,157 @@ const itemListSchema = new mongoose.Schema({
     required: true,
   },
   varients: [
-      {
-        title: {
-          type: String,
-          min: 1,
-          max: 255,
-          required: true,
-        },
-        price: {
-          type: Number,
-          min: 1,
-          max: 100000000000,
-          required: true,
-        },
-        cost: {
-          type: Number,
-          min: 1,
-          max: 100000000000,
-        },
-        SKU: {
-          type: String,
-          min: 1,
-          max: 100,
-        },
-        barcode: {
-          type: String,
-          min: 1,
-          max: 100000000000,
-        },
+    {
+      optionName: {
+        type: String,
+        min: 1,
+        max: 255,
+        required: true,
       },
-    ],
+      optionValue: [
+        {
+          title: {
+            type: String,
+            min: 1,
+            max: 255,
+            required: true,
+          },
+          price: {
+            type: Number,
+            min: 1,
+            max: 100000000000,
+            required: true,
+          },
+          cost: {
+            type: Number,
+            min: 1,
+            max: 100000000000,
+          },
+          SKU: {
+            type: String,
+            min: 1,
+            max: 100,
+          },
+          barcode: {
+            type: String,
+            min: 1,
+            max: 100000000000,
+          },
+        },
+      ],
+    },
+  ],
   stores: [
-      {
-        id: {
-          type: String,
-          min: 1,
-          max: 255,
-        },
-        title: {
-          type: String,
-          min: 1,
-          max: 255,
-        },
+    {
+      id: {
+        type: String,
+        min: 1,
+        max: 255,
       },
-    ],
+      title: {
+        type: String,
+        min: 1,
+        max: 255,
+      },
+
+      price: {
+        type: String,
+        min: 1,
+        max: 255,
+      },
+      inStock: {
+        type: String,
+        min: 1,
+        max: 255,
+      },
+      lowStock: {
+        type: String,
+        min: 1,
+        max: 255,
+      },
+      variantName: {
+        type: String,
+        min: 1,
+        max: 255,
+      },
+      modifiers: [
+        {
+          id: {
+            type: String,
+            min: 1,
+            max: 255,
+          },
+          title: {
+            type: String,
+            min: 1,
+            max: 255,
+          },
+        },
+      ],
+      taxes: [
+        {
+          id: {
+            type: String,
+            min: 1,
+            max: 255,
+          },
+          title: {
+            type: String,
+            min: 1,
+            max: 255,
+          },
+          type: {
+            type: String,
+            min: 1,
+            max: 255,
+          },
+          value: {
+            type: Number,
+            min: 1,
+            max: 1000000,
+          },
+        },
+      ],
+    },
+  ],
   modifiers: [
-      {
-        id: {
-          type: String,
-          min: 1,
-          max: 255,
-        },
-        title: {
-          type: String,
-          min: 1,
-          max: 255,
-        },
+    {
+      id: {
+        type: String,
+        min: 1,
+        max: 255,
       },
-    ],
+      title: {
+        type: String,
+        min: 1,
+        max: 255,
+      },
+    },
+  ],
   taxes: [
-      {
-        id: {
-          type: String,
-          min: 1,
-          max: 255,
-        },
-        title: {
-          type: String,
-          min: 1,
-          max: 255,
-        },
-        type: {
-          type: String,
-          min: 1,
-          max: 255,
-        },
-        value: {
-          type: Number,
-          min: 1,
-          max: 1000000,
-        }
+    {
+      id: {
+        type: String,
+        min: 1,
+        max: 255,
       },
-    ],
+      title: {
+        type: String,
+        min: 1,
+        max: 255,
+      },
+      type: {
+        type: String,
+        min: 1,
+        max: 255,
+      },
+      value: {
+        type: Number,
+        min: 1,
+        max: 1000000,
+      },
+    },
+  ],
   repoOnPos: {
     type: String,
     max: 14,
