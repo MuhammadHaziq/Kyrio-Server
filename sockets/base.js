803f22adaf0f6deb56a5dcd505d02636765937b5
Kyrio POS Server <tahiramjad79@gmail.com>
2021-03-18 06:15:40 +0000

    changes

--- sockets/base.js
+++ sockets/base.js
@@ -1,11 +1,11 @@
 module.exports = function (io) { 
     io.on('connection', function(socket) {
         console.log("connected")
-        socket.emit('announcements', { message: 'A new user has joined!' });
-        socket.on('itemUpdates', function(data,sock) {
-            console.log(data)
-            socket.emit('update', { message: 'Update Item on all devices!' });
-        });
+        // socket.emit('announcements', { message: 'A new user has joined!' });
+        // socket.on('itemUpdates', function(data,sock) {
+        //     console.log(data)
+        //     socket.emit('update', { message: 'Update Item on all devices!' });
+        // });
     });
     
  }                                                                                          
