a21dd881f4fe4c704217bbbb9ccb7b8c0f6d821e
TahirAmjad <tahiramjad79@gmail.com>
2022-03-27 14:34:37 +0100

    payment types and middlewares fixed

--- controllers/settings/paymentTypes/paymentMethods.js
+++ controllers/settings/paymentTypes/paymentMethods.js
@@ -21,12 +21,45 @@ router.post("/", async (req, res) => {
 });
 router.get("/", async (req, res) => {
   try {
-    const { account } = req.authData;
+    const { _id, account } = req.authData;
+    
     const result = await paymentMethods.find({
       account: account,
       title: { $ne: "Cash" },
     });
-    res.status(200).json(result);
+    if(result.length > 0){
+      res.status(200).json(result);
+    } else {
+      await paymentMethods
+        .create([
+          {
+            title: process.env.DEFAULT_PAYMENT_TYPES_1,
+            createdBy: _id,
+            account: account
+          },
+          {
+            title: process.env.DEFAULT_PAYMENT_TYPES_2,
+            createdBy: _id,
+            account: account
+          },
+          {
+            title: process.env.DEFAULT_PAYMENT_TYPES_3,
+            createdBy: _id,
+            account: account
+          },
+          {
+            title: process.env.DEFAULT_PAYMENT_TYPES_4,
+            createdBy: _id,
+            account: account
+          },
+        ]);
+        const result = await paymentMethods.find({
+          account: account,
+          title: { $ne: "Cash" },
+        });
+        res.status(200).json(result);
+    }
+    
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
